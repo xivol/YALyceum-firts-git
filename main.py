3b0db0f73b9d92c09a6e4685d3c0cd94f3e756ee
Admin4 <admin4@itcube61.ru>
2020-12-17 17:55:29 +0300

    included csv code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import sys
+import csv
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -23,3 +25,19 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+title = "nomen, definitio, pluma, Russian nomen,\
+ familia, Russian nomen familia".split(", ")
+
+lines = sys.stdin.readlines()
+
+csvfile = open('plantis.csv', 'w', newline='')
+writer = csv.writer(csvfile, delimiter=';', quotechar='"',
+                    quoting=csv.QUOTE_MINIMAL)
+
+writer.writerow(title)
+for line in lines:
+    writer.writerow(line.strip().split('\t'))
+
+csvfile.close()
